f5dd41bb5a9568c9cc733acb35ecfc3ab32be248
Marsraad_ <86083007+PanzerschokoladeAddict@users.noreply.github.com>
2026-05-30 18:33:36 -0800

    Code join

--- app/teams/join/codejoin.tsx
+++ app/teams/join/codejoin.tsx
@@ -1,3 +1,36 @@
+import * as Haptics from "expo-haptics";
+import { useRouter } from "expo-router";
+import React, { useState } from "react";
+import { Text } from "react-native";
+import { Button, TextInput } from "react-native-paper";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
 export default function CodeJoin() {
+    const [code, setCode] = useState("");
+    const router = useRouter();
     
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView>
+                <Text>
+                    Enter Team Code
+                </Text>
+                <TextInput
+                    label="Team Code"
+                    value={code}
+                    onChangeText={setCode}
+                >
+
+                </TextInput>
+                <Button
+                    onPress={() => {
+                        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+                        router.push({pathname: "./joined", params: { teamCode: code } });
+                    }}
+                >
+                    Join Team
+                </Button>
+            </SafeAreaView>
+        </SafeAreaProvider>
+    )
 }
